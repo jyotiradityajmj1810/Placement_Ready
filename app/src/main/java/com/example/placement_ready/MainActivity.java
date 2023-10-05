ee89c7ad3a552cb951a842e799bbbb83603f0ff3
jyotiradityajmj1810 <73978577+jyotiradityajmj1810@users.noreply.github.com>
2023-10-05 13:35:29 +0530

    adding reqired files

--- app/src/main/java/com/example/placement_ready/MainActivity.java
+++ app/src/main/java/com/example/placement_ready/MainActivity.java
@@ -1,14 +1,16 @@
-package com.example.placement_ready;
+  package com.example.placement_ready;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+  public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView titel = findViewById(R.id.firsttext);
     }
 }
