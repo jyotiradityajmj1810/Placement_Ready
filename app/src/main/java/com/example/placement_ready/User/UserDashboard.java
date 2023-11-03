8a18058432a0883cdb250581297358615aa50f88
Sambit Mohanty <sambit@localhost.localdomain>
2023-11-03 12:21:30 +0530

    adding project files

--- app/src/main/java/com/example/placement_ready/User/UserDashboard.java
+++ app/src/main/java/com/example/placement_ready/User/UserDashboard.java
@@ -1,22 +1,37 @@
 package com.example.placement_ready.User;
 
+import static com.example.placement_ready.R.id.drawer_layout;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.placement_ready.HelperClasses.Homeadepter.mostviewed_Adepter;
 import com.example.placement_ready.HelperClasses.Homeadepter.mostviewed_helperClass;
+import com.example.placement_ready.HelperClasses.Homeadepter.videos_Adepter;
 import com.example.placement_ready.R;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-public class UserDashboard extends AppCompatActivity {
-    RecyclerView mostviewed_recylerView;
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    RecyclerView mostviewed_recylerView,videos_recylerView;
     RecyclerView.Adapter adapter;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +40,62 @@ public class UserDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_user_dashboard);
 
         mostviewed_recylerView=findViewById(R.id.mostviewed_recylerView);
+        videos_recylerView=findViewById(R.id.videos_recylerView);
+        videos_recylerView();
         mostviewed_recylerView();
 
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        menuIcon = findViewById(R.id.menu_icon);
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+
+
+        naviagtionDrawer();
+
+    }
+
+    private void naviagtionDrawer() {
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+            else drawerLayout.closeDrawer(GravityCompat.START);
+        super.onBackPressed();
+    }
+
+    private void videos_recylerView() {
+        videos_recylerView.setHasFixedSize(true);
+        videos_recylerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<mostviewed_helperClass> videoslocation=new ArrayList<>();
+        videoslocation.add(new mostviewed_helperClass(R.drawable.btn1,"sfsf","dfghjkkfdffgf dgfhjkfgjgjgghjgjghghf fccgcccfghgh"));
+        videoslocation.add(new mostviewed_helperClass(R.drawable.cards,"kdfkd","dfsfsfsdfs"));
+        videoslocation.add(new mostviewed_helperClass(R.drawable.cards,"kdfkd","dfsfsfsdfs"));
+        videoslocation.add(new mostviewed_helperClass(R.drawable.cards,"kdfkd","dfsfsfsdfs"));
+        videoslocation.add(new mostviewed_helperClass(R.drawable.cards,"kdfkd","dfsfsfsdfs"));
+
+        adapter = new videos_Adepter(videoslocation);
+        videos_recylerView.setAdapter(adapter);
     }
 
     private void mostviewed_recylerView() {
@@ -40,11 +109,17 @@ public class UserDashboard extends AppCompatActivity {
         mostviewedlocation.add(new mostviewed_helperClass(R.drawable.cards,"kdfkd","dfsfsfsdfs"));
         mostviewedlocation.add(new mostviewed_helperClass(R.drawable.cards,"kdfkd","dfsfsfsdfs"));
         mostviewedlocation.add(new mostviewed_helperClass(R.drawable.cards,"kdfkd","dfsfsfsdfs"));
+
+
         adapter = new mostviewed_Adepter(mostviewedlocation);
         mostviewed_recylerView.setAdapter(adapter);
 
 
+    }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
 
     }
 }
