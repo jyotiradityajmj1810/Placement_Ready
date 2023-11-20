b5515662909760ae71bb4d7b09eb41c95cbc91a0
jyotiradityajmj1810 <73978577+jyotiradityajmj1810@users.noreply.github.com>
2023-11-20 18:48:36 +0530

    extra pages added

---
+++ app/src/main/java/com/example/placement_ready/Common/LoginSignup/OtpValidation.java
@@ -0,0 +1,16 @@
+package com.example.placement_ready.Common.LoginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.placement_ready.R;
+
+public class OtpValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_validation);
+    }
+}
